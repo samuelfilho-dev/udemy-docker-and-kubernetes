bfe54692c13fd6add241da206b0a2dfcdf73c5ab
samuel-filho-c9 <samuel@c9digital.com.br>
2025-04-18 19:25:47 -0300

    first commit

---
+++ yaml/app.py
@@ -0,0 +1,9 @@
+import yaml
+
+if __name__ == "__main__":
+    stream = open("config.yaml", "r")
+    dictionary = yaml.safe_load(stream)
+
+    for key, value in dictionary.items():
+        print(f"{key}: {value}")
+    stream.close()
